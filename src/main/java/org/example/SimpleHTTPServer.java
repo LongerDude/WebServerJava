583b3fc163588ef4585ac7b7a64fab5d5aa13042
Salmane Khalili <salmajo2001@gmail.com>
2025-10-07 16:21:52 +0100

    implemented logic for server and clients

---
+++ src/main/java/org/example/SimpleHTTPServer.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleHTTPServer{
+    public static void main(String[] args) throws Exception{
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening on port 8080");
+        while(true){
+            final Socket client = server.accept();
+            InputStreamReader isr = new InputStreamReader(client.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+            while(!line.isEmpty()) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+
+        }
+    }
+    
+}
